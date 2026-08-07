90ab6a3faea454e94dff9b7f4bbfd3ded27884a1
Hellayne <gomeshelayne890@gmail.com>
2026-08-07 12:31:47 -0300

    feat: componentes e app feito, e react router configurado

---
+++ frontend/src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,10 @@
+import { SideBar } from "../SideBar/SideBar.jsx";
+
+export const MainLayout  = () => {
+  return (
+    <main>
+        <h1>preencher quando o figma estiver pronto.</h1>
+        <SideBar />
+    </main>
+  );
+}
---
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -0,0 +1,9 @@
+export const SideBar = () =>{
+    return(
+        <aside>
+            <div>
+                <h3>caixa com coisa.</h3>
+            </div>
+        </aside>
+    );
+}
