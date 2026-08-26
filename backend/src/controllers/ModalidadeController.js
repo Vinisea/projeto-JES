df2f99d1669fff9a7fd06ac4f61c88324c7c026c
Pietro <pietrocontas51@gmail.com>
2026-08-26 12:18:59 -0300

    feat: adiciona estrutura de grupos e partidas

--- backend/src/controllers/ModalidadeController.js
+++ backend/src/controllers/ModalidadeController.js
@@ -3,64 +3,64 @@ import { modalidade } from "../models/Modalidade.js";
 // TODO: impedir cadastro de modalidades com mesmo nome/categoria //
 // -------------------------------------------------------------- //
 export const listarModalidades = async (req, res, next) => {
-    try {
-        const modalidades = await modalidade.findAll();
-        return res.status(200).json(modalidades);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const modalidades = await modalidade.findAll();
+    return res.status(200).json(modalidades);
+  } catch (error) {
+    next(error);
+  }
 };
 export const buscarModalidadePorId = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const modalidadeEncontrada = await modalidade.findByPk(id);
-        if (!modalidadeEncontrada) {
-            return res.status(404).json({
-                message: "Modalidade não encontrada"
-            });
-        }
-        return res.status(200).json(modalidadeEncontrada);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { id } = req.params;
+    const modalidadeEncontrada = await modalidade.findByPk(id);
+    if (!modalidadeEncontrada) {
+      return res.status(404).json({
+        message: "Modalidade não encontrada",
+      });
+    }
+    return res.status(200).json(modalidadeEncontrada);
+  } catch (error) {
+    next(error);
+  }
 };
 export const criarModalidade = async (req, res, next) => {
-    try {
-        const novaModalidade = await modalidade.create(req.body);
-        return res.status(201).json(novaModalidade);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const novaModalidade = await modalidade.create(req.body);
+    return res.status(201).json(novaModalidade);
+  } catch (error) {
+    next(error);
+  }
 };
 export const editarModalidade = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const modalidadeEncontrada = await modalidade.findByPk(id);
-        if (!modalidadeEncontrada) {
-            return res.status(404).json({
-                message: "Modalidade não encontrada"
-            });
-        }
-        await modalidadeEncontrada.update(req.body);
-        return res.status(200).json(modalidadeEncontrada);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { id } = req.params;
+    const modalidadeEncontrada = await modalidade.findByPk(id);
+    if (!modalidadeEncontrada) {
+      return res.status(404).json({
+        message: "Modalidade não encontrada",
+      });
+    }
+    await modalidadeEncontrada.update(req.body);
+    return res.status(200).json(modalidadeEncontrada);
+  } catch (error) {
+    next(error);
+  }
 };
 export const removerModalidade = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const modalidadeEncontrada = await modalidade.findByPk(id);
-        if (!modalidadeEncontrada) {
-            return res.status(404).json({
-                message: "Modalidade não encontrada"
-            });
-        }
-        await modalidadeEncontrada.destroy();
-        return res.status(204).send();
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const { id } = req.params;
+    const modalidadeEncontrada = await modalidade.findByPk(id);
+    if (!modalidadeEncontrada) {
+      return res.status(404).json({
+        message: "Modalidade não encontrada",
+      });
+    }
+    await modalidadeEncontrada.destroy();
+    return res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
 
 //oi
