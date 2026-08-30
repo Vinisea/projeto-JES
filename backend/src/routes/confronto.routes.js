1448e1abd2df64a5d47eca77765726f96d716d4d
vinisea <viniciusafseabra@gmail.com>
2026-08-30 17:01:38 -0300

    feat: rotas confronto completas

--- backend/src/routes/confronto.routes.js
+++ backend/src/routes/confronto.routes.js
@@ -6,27 +6,21 @@ import {
     criarConfronto,
     editarConfronto,
     removerConfronto,
-    // iniciarConfronto,
-    // encerrarConfronto,
-    // atualizarPlacar
+    atualizarPlacar,
+    finalizarConfronto,
+    iniciarConfronto
 } from "../controllers/ConfrontoController.js";
 
 const router = Router();
 
 router.get("/", listarConfrontos);
-
 router.get("/:id", buscarConfrontoPorId);
-
 router.post("/", criarConfronto);
-
 router.put("/:id", editarConfronto);
-
 router.delete("/:id", removerConfronto);
 
-// router.patch("/:id/iniciar", iniciarConfronto);
-
-// router.patch("/:id/encerrar", encerrarConfronto);
-
-// router.patch("/:id/placar", atualizarPlacar);
+router.patch("/:id/iniciar", iniciarConfronto);
+router.patch("/:id/finalizar", finalizarConfronto);
+router.patch("/:id/placar", atualizarPlacar);
 
 export default router;
